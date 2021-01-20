0e524499614c53e1cdbab5ff83015d24ab1e40fc
Sergey Khotynuyk <hot22muz@gmail.com>
2021-01-20 19:25:10 +0300

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        String s = "JAVA ";
+        int val = 0;
+        while (val < 10){
+            System.out.print(s);
+            val++;
+        }
     }
 }
--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task4 {
     public static void main(String[] args) {
-
+        int value  = 1980;
+        while (value <= 2020){
+            System.out.println("Олимпиада " + value + " года");
+            value = value + 4;
+        }
     }
 }
