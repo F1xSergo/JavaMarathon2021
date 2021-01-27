7802bfaab6501ab7e38b1172a90ddf181f98e78e
Sergey Khotynuyk <hot22muz@gmail.com>
2021-01-27 21:31:52 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        String string = "";
+        for (int i = 0; i <= 20_000;i++) {
+            string = string + i + " "; // почему + выделяет желтым
+            //string = String.format("%s%d ", string, i);
+        }
+        System.out.println(string);
+        long finish = System.currentTimeMillis();
+        System.out.println(finish - start + " ms");
+
+        long start2 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 20_000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString());
+        long finish2 = System.currentTimeMillis();
+        System.out.println(finish2 - start2 + " ms");
+
 
     }
 }
